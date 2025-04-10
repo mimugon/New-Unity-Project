57a51d5886aa1aca3a0ba19e484a65709cebf91f
mimugon <49438383@est.ort.edu.ar>
2025-04-10 12:09:39 -0300

    Simulacro Completado

---
+++ Assets/Script/HeladeriaPedidos.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeladeriaPedidos : MonoBehaviour
+{
+
+    public string SaborDeHelado;
+    public int CantidadHelado;
+    int Descuento;
+    float PrecioHelado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      if (CantidadHelado < 250 || CantidadHelado >3000)
+        {
+            Debug.Log("Cantidad de helado no valida");
+            return;
+        }
+        if (SaborDeHelado != "CHO" && SaborDeHelado != "DDL" && SaborDeHelado != "FRU")
+        {
+            Debug.Log("Codigo no valido");
+            return;
+        }
+
+        PrecioHelado = CantidadHelado * 0.5f;
+
+        if(SaborDeHelado == "FRU")
+        {
+            PrecioHelado = PrecioHelado - PrecioHelado * 0.1f;
+        }
+
+        Debug.Log("Debe abonar " + PrecioHelado + " pesos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
